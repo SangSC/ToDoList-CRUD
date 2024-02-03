24b75c7be5eb34a8f3906e9c2e3aab3020020a3f
SangSC <r.sang.chao@icloud.com>
2024-02-03 23:28:03 +0800

    ongoing

--- src/components/DeleteTask.tsx
+++ src/components/DeleteTask.tsx
@@ -1,7 +1,26 @@
 import { useRouter } from "next/navigation";
 import { Trash2 } from "lucide-react";
+import toast, { Toaster } from "react-hot-toast";
 
 import { API_URL } from "../lib/API_URL";
+
+const notify = () =>
+  toast(
+    (t) => (
+      <span>
+        Are you sure you want to delete?
+        <button
+          className="border-2 border-black border-opacity-50 rounded px-2 ml-4 "
+          onClick={() => toast.dismiss(t.id)}
+        >
+          OK
+        </button>
+      </span>
+    ),
+    {
+      icon: <Trash2 className="text-red-500" />,
+    }
+  );
 
 const DeleteTask = () => {
   const router = useRouter();
@@ -22,11 +41,15 @@ const DeleteTask = () => {
     <div className="text-center">
       <button
         title="Delete"
-        onClick={handleDelete}
+        onClick={(event) => {
+          handleDelete(event);
+          notify();
+        }}
         className="del-btn flex justify-center items-center"
       >
         <Trash2 className="w-[14px] h-[14px]" />
       </button>
+      <Toaster />
     </div>
   );
 };
